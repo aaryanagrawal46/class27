176dbdb567957c50cd018f55c045ab04131efda4
aaryanagrawal46 <aaryanagrawal2006@gmail.com>
2021-01-28 17:54:12 +0530

    log6.bird

--- sketch.js
+++ sketch.js
@@ -6,7 +6,8 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-var constrainedLog,chain;
+var log6,chain1;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,9 +18,10 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    constrainedLog= new Log(230,180,80,PI/2);
+    
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -37,15 +39,11 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-    var options = {
-        bodyA:bird.body,
-        bodyB:constrainedLog.body,
-        stiffness: 0.04,
-        length:10
-    }
-    var chain = Constraint.create(options);
+    log6= new Log(230,180,80,PI/2);
+    chain1=new chain(bird.body,log6.body);
+    
 
-    World.add(world,chain);
+    
 }
 
 function draw(){
@@ -71,8 +69,7 @@ function draw(){
 
     bird.display();
     platform.display();
-    constrainedLog.display();
-
-    strokeWeight(3);
-    line(bird.body.position.x,bird.body.position.y,constrainedLog.body.position.x,constrainedLog.body.position.y);
+    log6.display();
+    chain1.display();
 }
+
